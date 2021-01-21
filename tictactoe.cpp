5935266b7687a03e56896bde211e884c777a7b90
jonetxe13 <jonetxe13@DESKTOP-LBMEVES.localdomain>
2021-01-21 18:30:44 +0100

    Add existing file

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 
 using namespace std;
@@ -8,15 +9,32 @@ bool turnoDeX = false;
 
 char tablero[][3] = {{'_', '_', '_'},{'_', '_', '_'},{'_', '_', '_'}};
 
+
+char jugador(){
+
+        char player;
+	if (turnoDeX == false){ player = 'X';}
+	else if (turnoDeX == true){player = 'O';}
+	else {player = ' ';}
+	return player;
+}
+
+void situacion(){
+	if (tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0]){cout << "has ganado\n";}
+	else if (tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1]){cout << "has ganado\n";}
+	else if (tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]){cout << "has ganado\n";}
+	else if (tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2]){cout << "has ganado\n";}
+	else if (tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2]){cout << "has ganado\n";}
+	else if (tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2]){cout << "has ganado\n";}
+	else if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]){cout << "has ganado\n";}
+	else if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]){cout << "has ganado\n";}
+	else {cout << "siguiente movimiento\n";}
+}
+
+
 void DibujarTablero(){
 	
-        char jugador;
-	
-	if (turnoDeX == false){ jugador = 'X';}
-	else if (turnoDeX == true){jugador = 'O';}
-	else {jugador = ' ';}
-
-	int fila;
+        int fila;
         int columna;
      
         for(int i = 0; i < 3; i++){
@@ -29,14 +47,21 @@ void DibujarTablero(){
 	cin >> fila;
 	cin >> columna;
         
-	tablero[fila][columna] = jugador;
+	tablero[fila][columna] = jugador();
         turnoDeX = !turnoDeX;
 }
 int main(){
 
 	while(true){
 		DibujarTablero();
+		jugador();
+		situacion();
 	}
+	char stop;
+	cin << stop;
+	if(stop == ' '){
+		break;
+	} 
 	return 0;
 }
 
